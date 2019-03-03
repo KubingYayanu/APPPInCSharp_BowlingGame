3589bb72498e6c4e217a57fcc9641937f77194ea
Kubing Lin <edkt973@gmail.com>
2019-03-03 23:11:37 +0800

    add GameTests

---
+++ BowlingGame.Console/Game.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BowlingGame.Console
+{
+    public class Game
+    {
+        private int score;
+        private int[] throws = new int[21];
+        private int currentThrow;
+
+        public int Score => score;
+
+        public void Add(int pins)
+        {
+            throws[currentThrow++] = pins;
+            score += pins;
+        }
+
+        public int ScoreForFrame(int theFrame)
+        {
+            int ball = 0;
+            int score = 0;
+            for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
+            {
+                int firstThrow = throws[ball++];
+                int secondThrow = throws[ball++];
+                score += firstThrow + secondThrow;
+            }
+
+            return score;
+        }
+    }
+}
---
+++ BowlingGame.UnitTests/GameTests.cs
@@ -0,0 +1,59 @@
+﻿using BowlingGame.Console;
+using NUnit.Framework;
+
+namespace BowlingGame.UnitTests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        private Game game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void Add_OneThrowNoMark_Return5()
+        {
+            game.Add(5);
+            Assert.That(game.Score, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void Add_TwoThrowsNoMark_Return9()
+        {
+            game.Add(5);
+            game.Add(4);
+            Assert.That(game.Score, Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Add_OneFrameNoMark_Return9()
+        {
+            game.Add(5);
+            game.Add(4);
+            Assert.That(game.ScoreForFrame(1), Is.EqualTo(9));
+        }
+
+        [Test]
+        public void Add_TwoFramesNoMark_Return18()
+        {
+            game.Add(5);
+            game.Add(4);
+            game.Add(7);
+            game.Add(2);
+            Assert.That(game.ScoreForFrame(2), Is.EqualTo(18));
+        }
+
+        [Test]
+        public void Add_SimpleSpare_Return13()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            Assert.That(game.ScoreForFrame(1), Is.EqualTo(13));
+        }
+    }
+}
