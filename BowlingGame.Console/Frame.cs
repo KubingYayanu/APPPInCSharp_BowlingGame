d364eff6dab092ba6d0516fb682398f4e042a773
Kubing Lin <edkt973@gmail.com>
2019-03-03 23:11:26 +0800

    add FrameTests

---
+++ BowlingGame.Console/Frame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BowlingGame.Console
+{
+    public class Frame
+    {
+        private int score;
+
+        public int Score => score;
+
+        public void Add(int pins)
+        {
+            score += pins;
+        }
+    }
+}
---
+++ BowlingGame.UnitTests/FrameTests.cs
@@ -0,0 +1,24 @@
+﻿using BowlingGame.Console;
+using NUnit.Framework;
+
+namespace BowlingGame.UnitTests
+{
+    [TestFixture]
+    public class FrameTests
+    {
+        [Test]
+        public void Score_NoThrows_Return0()
+        {
+            var frame = new Frame();
+            Assert.That(frame.Score, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Add_OneThrowNoMark_Return5()
+        {
+            var frame = new Frame();
+            frame.Add(5);
+            Assert.That(frame.Score, Is.EqualTo(5));
+        }
+    }
+}
