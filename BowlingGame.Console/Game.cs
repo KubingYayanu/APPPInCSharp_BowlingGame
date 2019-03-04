ebd3f6c4d44918da6d316ec20d15491abe6ba12a
Kubing Lin <edkt973@gmail.com>
2019-03-04 19:51:10 +0800

    refactoring Game

--- BowlingGame.Console/Game.cs
+++ BowlingGame.Console/Game.cs
@@ -6,11 +6,18 @@
         private int[] throws = new int[21];
         private int currentThrow;
         private int currentFrame = 1;
+        private int ball;
         private bool IsFirstThrow = true;
 
         public int Score => ScoreForFrame(CurrentFrame - 1);
 
         public int CurrentFrame => currentFrame;
+
+        private int NextTwoBallsForStrike => throws[ball + 1] + throws[ball + 2];
+
+        private int NextBallForSpare => throws[ball + 2];
+
+        private int TwoBallsInFrame => throws[ball] + throws[ball + 1];
 
         public void Add(int pins)
         {
@@ -46,32 +53,38 @@
 
         public int ScoreForFrame(int theFrame)
         {
-            int ball = 0;
+            ball = 0;
             int score = 0;
             for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
             {
-                int firstThrow = throws[ball++];
-                //Strike
-                if (firstThrow == 10)
+                if (Strike())
                 {
-                    score += 10 + throws[ball] + throws[ball + 1];
+                    score += 10 + NextTwoBallsForStrike;
+                    ball++;
+                }
+                else if (Spare())
+                {
+                    score += 10 + NextBallForSpare;
+                    ball += 2;
                 }
                 else
                 {
-                    int secondThrow = throws[ball++];
-                    int frameScore = firstThrow + secondThrow;
-
-                    //Spare
-                    if (frameScore == 10)
-                    {
-                        score += frameScore + throws[ball];
-                    }
-                    else
-                        score += frameScore;
+                    score += TwoBallsInFrame;
+                    ball += 2;
                 }
             }
 
             return score;
+        }
+
+        private bool Strike()
+        {
+            return throws[ball] == 10;
+        }
+
+        private bool Spare()
+        {
+            return throws[ball] + throws[ball + 1] == 10;
         }
     }
 }
