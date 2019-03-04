59686768f2c0441168eba5761e6bc2eeb45ec309
Kubing Lin <edkt973@gmail.com>
2019-03-04 20:36:36 +0800

    refactoring Game and GameTests

--- BowlingGame.Console/Game.cs
+++ BowlingGame.Console/Game.cs
@@ -2,89 +2,52 @@
 {
     public class Game
     {
-        private int score;
-        private int[] throws = new int[21];
-        private int currentThrow;
         private int currentFrame = 1;
-        private int ball;
         private bool IsFirstThrow = true;
+        private Scorer scorer = new Scorer();
 
-        public int Score => ScoreForFrame(CurrentFrame - 1);
-
-        public int CurrentFrame => currentFrame;
-
-        private int NextTwoBallsForStrike => throws[ball + 1] + throws[ball + 2];
-
-        private int NextBallForSpare => throws[ball + 2];
-
-        private int TwoBallsInFrame => throws[ball] + throws[ball + 1];
+        public int Score => ScoreForFrame(currentFrame);
 
         public void Add(int pins)
         {
-            throws[currentThrow++] = pins;
-            score += pins;
-
+            scorer.AddThrow(pins);
             AdjustCurrentFrame(pins);
-        }
-
-        private void AdjustCurrentFrame(int pins)
-        {
-            if (IsFirstThrow)
-            {
-                //Strike
-                if (pins == 10)
-                {
-                    currentFrame++;
-                }
-                else
-                    IsFirstThrow = false;
-            }
-            else
-            {
-                IsFirstThrow = true;
-                currentFrame++;
-            }
-
-            if (currentFrame > 11)
-            {
-                currentFrame = 11;
-            }
         }
 
         public int ScoreForFrame(int theFrame)
         {
-            ball = 0;
-            int score = 0;
-            for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
+            return scorer.ScoreForFrame(theFrame);
+        }
+
+        private void AdjustCurrentFrame(int pins)
+        {
+            if (LastBallInFrame(pins))
             {
-                if (Strike())
-                {
-                    score += 10 + NextTwoBallsForStrike;
-                    ball++;
-                }
-                else if (Spare())
-                {
-                    score += 10 + NextBallForSpare;
-                    ball += 2;
-                }
-                else
-                {
-                    score += TwoBallsInFrame;
-                    ball += 2;
-                }
+                AdvanceFrame();
             }
-
-            return score;
+            else
+            {
+                IsFirstThrow = false;
+            }
         }
 
-        private bool Strike()
+        private void AdvanceFrame()
         {
-            return throws[ball] == 10;
+            currentFrame++;
+            if (currentFrame > 10)
+            {
+                currentFrame = 10;
+            }
         }
 
-        private bool Spare()
+        private bool Strike(int pins)
         {
-            return throws[ball] + throws[ball + 1] == 10;
+            return IsFirstThrow && pins == 10;
+        }
+
+        private bool LastBallInFrame(int pins)
+        {
+            return Strike(pins) || !IsFirstThrow;
         }
     }
 }
