769d0920bdbe3466c51dcc1ab53813deba9305e4
Kubing Lin <edkt973@gmail.com>
2019-03-04 14:38:01 +0800

    refactoring Game and GameTests

--- BowlingGame.Console/Game.cs
+++ BowlingGame.Console/Game.cs
@@ -1,19 +1,36 @@
-﻿using System;
-
-namespace BowlingGame.Console
+﻿namespace BowlingGame.Console
 {
     public class Game
     {
         private int score;
         private int[] throws = new int[21];
         private int currentThrow;
+        private int currentFrame = 1;
+        private bool IsFirstThrow = true;
 
-        public int Score => score;
+        public int Score => ScoreForFrame(CurrentFrame -1);
+
+        public int CurrentFrame => currentFrame;
 
         public void Add(int pins)
         {
             throws[currentThrow++] = pins;
             score += pins;
+
+            AdjustCurrentFrame();
+        }
+
+        private void AdjustCurrentFrame()
+        {
+            if (IsFirstThrow)
+            {
+                IsFirstThrow = false;
+            }
+            else
+            {
+                IsFirstThrow = true;
+                currentFrame++;
+            }
         }
 
         public int ScoreForFrame(int theFrame)
@@ -24,7 +41,14 @@ namespace BowlingGame.Console
             {
                 int firstThrow = throws[ball++];
                 int secondThrow = throws[ball++];
-                score += firstThrow + secondThrow;
+                int frameScore = firstThrow + secondThrow;
+
+                if (frameScore == 10)
+                {
+                    score += frameScore + throws[ball];
+                }
+                else
+                    score += frameScore;
             }
 
             return score;
--- BowlingGame.UnitTests/GameTests.cs
+++ BowlingGame.UnitTests/GameTests.cs
@@ -14,23 +14,29 @@ namespace BowlingGame.UnitTests
             game = new Game();
         }
 
+        #region Add
+
         [Test]
-        public void Add_OneThrowNoMark_Return5()
+        public void Add_OneThrowNoMark_ReturnTotalScore5()
         {
             game.Add(5);
             Assert.That(game.Score, Is.EqualTo(5));
         }
 
         [Test]
-        public void Add_TwoThrowsNoMark_Return9()
+        public void Add_TwoThrowsNoMark_ReturnTotalScore9()
         {
             game.Add(5);
             game.Add(4);
             Assert.That(game.Score, Is.EqualTo(9));
         }
 
+        #endregion Add
+
+        #region ScoreForFrame
+
         [Test]
-        public void Add_OneFrameNoMark_Return9()
+        public void ScoreForFrame_Frame1NoMark_ReturnFrameScore9()
         {
             game.Add(5);
             game.Add(4);
@@ -38,7 +44,7 @@ namespace BowlingGame.UnitTests
         }
 
         [Test]
-        public void Add_TwoFramesNoMark_Return18()
+        public void ScoreForFrame_Frame2NoMark_ReturnFrameScoreIs18()
         {
             game.Add(5);
             game.Add(4);
@@ -48,12 +54,82 @@ namespace BowlingGame.UnitTests
         }
 
         [Test]
-        public void Add_SimpleSpare_Return13()
+        public void ScoreForFrame_Frame1AfterSimpleSpare_ReturnFrameScoreIs13()
         {
             game.Add(3);
             game.Add(7);
             game.Add(3);
             Assert.That(game.ScoreForFrame(1), Is.EqualTo(13));
         }
+
+        [Test]
+        public void ScoreForFrame_Frame2AfterSimpleSpare_ReturnFrameScoreIs18()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.That(game.ScoreForFrame(2), Is.EqualTo(18));
+        }
+
+        [Test]
+        public void Score_SimpleFrameAfterSpare_ReturnTotalScoreIs18()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.That(game.Score, Is.EqualTo(18));
+        }
+
+        #endregion ScoreForFrame
+
+        #region CurrentFrame
+
+        [Test]
+        public void CurrentFrame_OneThrowNoMark_ReturnCurrentFrameIs1()
+        {
+            game.Add(5);
+            Assert.That(game.CurrentFrame, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void CurrentFrame_TwoThrowsNoMark_ReturnCurrentFrameIs1()
+        {
+            game.Add(5);
+            game.Add(4);
+            Assert.That(game.CurrentFrame, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CurrentFrame_FourThrowsNoMark_ReturnCurrentFrameIs2()
+        {
+            game.Add(5);
+            game.Add(4);
+            game.Add(7);
+            game.Add(2);
+            Assert.That(game.CurrentFrame, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void CurrentFrame_SimpleSpare_ReturnCurrentFrameIs2()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            Assert.That(game.CurrentFrame, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CurrentFrame_SimpleFrameAfterSpare_ReturnCurrentFrameIs3()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.That(game.CurrentFrame, Is.EqualTo(3));
+        }
+        
+        #endregion CurrentFrame
     }
 }
