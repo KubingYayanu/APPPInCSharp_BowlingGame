68b6fd9b54bcca8a88501a6b81505242f9a802b7
Kubing Lin <edkt973@gmail.com>
2019-03-04 15:37:47 +0800

    refactoring Game and GameTests

--- BowlingGame.Console/Game.cs
+++ BowlingGame.Console/Game.cs
@@ -8,7 +8,7 @@
         private int currentFrame = 1;
         private bool IsFirstThrow = true;
 
-        public int Score => ScoreForFrame(CurrentFrame -1);
+        public int Score => ScoreForFrame(CurrentFrame - 1);
 
         public int CurrentFrame => currentFrame;
 
@@ -17,19 +17,30 @@
             throws[currentThrow++] = pins;
             score += pins;
 
-            AdjustCurrentFrame();
+            AdjustCurrentFrame(pins);
         }
 
-        private void AdjustCurrentFrame()
+        private void AdjustCurrentFrame(int pins)
         {
             if (IsFirstThrow)
             {
-                IsFirstThrow = false;
+                //Strike
+                if (pins == 10)
+                {
+                    currentFrame++;
+                }
+                else
+                    IsFirstThrow = false;
             }
             else
             {
                 IsFirstThrow = true;
                 currentFrame++;
+            }
+
+            if (currentFrame > 11)
+            {
+                currentFrame = 11;
             }
         }
 
@@ -40,15 +51,24 @@
             for (int currentFrame = 0; currentFrame < theFrame; currentFrame++)
             {
                 int firstThrow = throws[ball++];
-                int secondThrow = throws[ball++];
-                int frameScore = firstThrow + secondThrow;
-
-                if (frameScore == 10)
+                //Strike
+                if (firstThrow == 10)
                 {
-                    score += frameScore + throws[ball];
+                    score += 10 + throws[ball] + throws[ball + 1];
                 }
                 else
-                    score += frameScore;
+                {
+                    int secondThrow = throws[ball++];
+                    int frameScore = firstThrow + secondThrow;
+
+                    //Spare
+                    if (frameScore == 10)
+                    {
+                        score += frameScore + throws[ball];
+                    }
+                    else
+                        score += frameScore;
+                }
             }
 
             return score;
--- BowlingGame.UnitTests/GameTests.cs
+++ BowlingGame.UnitTests/GameTests.cs
@@ -17,13 +17,6 @@ namespace BowlingGame.UnitTests
         #region Add
 
         [Test]
-        public void Add_OneThrowNoMark_ReturnTotalScore5()
-        {
-            game.Add(5);
-            Assert.That(game.Score, Is.EqualTo(5));
-        }
-
-        [Test]
         public void Add_TwoThrowsNoMark_ReturnTotalScore9()
         {
             game.Add(5);
@@ -32,6 +25,102 @@ namespace BowlingGame.UnitTests
         }
 
         #endregion Add
+
+        #region Score
+
+        [Test]
+        public void Score_SimpleFrameAfterSpare_ReturnTotalScoreIs18()
+        {
+            game.Add(3);
+            game.Add(7);
+            game.Add(3);
+            game.Add(2);
+            Assert.That(game.Score, Is.EqualTo(18));
+        }
+
+        [Test]
+        public void Score_SimpleStrike_ReturnTotalScoreIs28()
+        {
+            game.Add(10);
+            game.Add(3);
+            game.Add(6);
+            Assert.That(game.Score, Is.EqualTo(28));
+        }
+
+        [Test]
+        public void Score_PerfectGame_ReturnTotalScoreIs300()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Add(10);
+            }
+            Assert.That(game.Score, Is.EqualTo(300));
+        }
+
+        [Test]
+        public void Score_EndOfArray_ReturnTotalScoreIs20()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                game.Add(0);
+                game.Add(0);
+            }
+            game.Add(2);
+            game.Add(8);
+            game.Add(10);
+            Assert.That(game.Score, Is.EqualTo(20));
+        }
+
+        [Test]
+        public void Score_SampleGame_ReturnTotalScoreIs133()
+        {
+            game.Add(1);
+            game.Add(4);
+            game.Add(4);
+            game.Add(5);
+            game.Add(6);
+            game.Add(4);
+            game.Add(5);
+            game.Add(5);
+            game.Add(10);
+            game.Add(0);
+            game.Add(1);
+            game.Add(7);
+            game.Add(3);
+            game.Add(6);
+            game.Add(4);
+            game.Add(10);
+            game.Add(2);
+            game.Add(8);
+            game.Add(6);
+            Assert.That(game.Score, Is.EqualTo(133));
+        }
+
+        [Test]
+        public void Score_AlmostPerfectGame_ReturnTotalScoreIs299()
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                game.Add(10);
+            }
+            game.Add(9);
+            Assert.That(game.Score, Is.EqualTo(299));
+        }
+
+        [Test]
+        public void Score_Frame10Spare_ReturnTotalScoreIs270()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                game.Add(10);
+            }
+            game.Add(9);
+            game.Add(1);
+            game.Add(1);
+            Assert.That(game.Score, Is.EqualTo(270));
+        }
+
+        #endregion Score
 
         #region ScoreForFrame
 
@@ -73,13 +162,12 @@ namespace BowlingGame.UnitTests
         }
 
         [Test]
-        public void Score_SimpleFrameAfterSpare_ReturnTotalScoreIs18()
+        public void ScoreForFrame_Frame1AfterSimpleStrike_ReturnFrameScoreIs19()
         {
+            game.Add(10);
             game.Add(3);
-            game.Add(7);
-            game.Add(3);
-            game.Add(2);
-            Assert.That(game.Score, Is.EqualTo(18));
+            game.Add(6);
+            Assert.That(game.ScoreForFrame(1), Is.EqualTo(19));
         }
 
         #endregion ScoreForFrame
@@ -129,7 +217,26 @@ namespace BowlingGame.UnitTests
             game.Add(2);
             Assert.That(game.CurrentFrame, Is.EqualTo(3));
         }
-        
+
+        [Test]
+        public void CurrentFrame_SimpleStrike_ReturnCurrentFrameIs3()
+        {
+            game.Add(10);
+            game.Add(3);
+            game.Add(6);
+            Assert.That(game.CurrentFrame, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void CurrentFrame_PerfectGame_ReturnCurrentFrameIs10()
+        {
+            for (int i = 0; i < 12; i++)
+            {
+                game.Add(10);
+            }
+            Assert.That(game.CurrentFrame, Is.EqualTo(11));
+        }
+
         #endregion CurrentFrame
     }
 }
